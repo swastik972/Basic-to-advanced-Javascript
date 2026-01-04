22ecbdc634edfe3c6e631516ffe5de033ec77bbb
swastik972 <rawatswastik30@gmail.com>
2026-01-04 11:01:16 +0545

    changes commit

--- day2/thisAndBinding.js
+++ day2/thisAndBinding.js
@@ -1,33 +1,35 @@
-// this always refers to the nearest object or instant
+// // this always refers to the nearest object or instant
 
-let obj1 = {
-    price: 400,
-    fruitName : "apple",
-    weight: 2,
-    unit : "kg",
+// let obj1 = {
+//     price: 400,
+//     fruitName : "apple",
+//     weight: 2,
+//     unit : "kg",
     
-    totalCost: function(){
-        let totalPrice = this.price*this.weight;
-        console.log("inside total cost function: ", totalPrice)
-    }
-    //,
-    //  displayFruitName: function()=>{
-       //this keyword will never exist in the arrow function
-    //     console.log("Fruit Name: ", this.fruitName);
-    //  }
-}
+//     totalCost: function(){
+//         let totalPrice = this.price*this.weight;
+//         console.log("inside total cost function: ", totalPrice)
+//     }
+//     //,
+//     //  displayFruitName: function()=>{
+//        //this keyword will never exist in the arrow function
+//     //     console.log("Fruit Name: ", this.fruitName);
+//     //  }
+// }
 
-//calling totalCost function
-obj1.totalCost()
+// //calling totalCost function
+// obj1.totalCost()
 
-//assigning totalCost function a variable here when we call the function through variable, this will be lost.
+// //assigning totalCost function a variable here when we call the function through variable, this will be lost.
 
-let myNewFunction = obj1.totalCost
-myNewFunction()
+// let myNewFunction = obj1.totalCost
+// myNewFunction()
 
-//using bind to preserve this keyboard
-let myNewFunctionwithbind = obj1.totalCost.bind(obj1)
-myNewFunctionwithbind()
-//calling arrow function
-obj1.displayFruitName()
+// //using bind to preserve this keyboard
+// let myNewFunctionwithbind = obj1.totalCost.bind(obj1)
+// myNewFunctionwithbind()
+// //calling arrow function
+// obj1.displayFruitName()
+
+
 
