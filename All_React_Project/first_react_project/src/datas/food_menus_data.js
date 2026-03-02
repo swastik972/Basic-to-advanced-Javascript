ef4a21749038609410067dc9b8dfc9ad708d3d13
swastik972 <rawatswastik30@gmail.com>
2026-03-02 21:50:33 +0545

    Update food_menus_data.js

--- All_React_Project/first_react_project/src/datas/food_menus_data.js
+++ All_React_Project/first_react_project/src/datas/food_menus_data.js
@@ -13,5 +13,13 @@ export let foodMenuList = [
         description: "A delicious chowmein with your choice of vegetables.",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9CJte3to9KEkTzguPBfRGZCgCeWjmopK0TQ&s",
     },
+    {
+        id: 3,
+        name: "Pizza",
+        price: 1500,
+        description: "A delicious pizza with your choice of toppings.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9CJte3to9KEkTzguPBfRGZCgCeWjmopK0TQ&s",
+    }
+
 
 ]
